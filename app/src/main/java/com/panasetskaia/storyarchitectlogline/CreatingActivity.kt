8523b1167eedc5768fda0792c43e40622583dcbb
Maria Panasetskaia <mariapolikutina@gmail.com>
2023-01-22 14:09:15 +0800

    add dependencies

--- app/src/main/java/com/panasetskaia/storyarchitectlogline/CreatingActivity.kt
+++ app/src/main/java/com/panasetskaia/storyarchitectlogline/CreatingActivity.kt
@@ -7,5 +7,12 @@ class CreatingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_creating)
+
+//        val wormDotsIndicator = findViewById(R.id.worm_dots_indicator)
+//        val viewPager = findViewById<ViewPager>(R.id.view_pager)
+//        val adapter = ViewPagerAdapter()
+//        viewPager.adapter = adapter
+//        wormDotsIndicator.attachTo(viewPager)
+
     }
 }
